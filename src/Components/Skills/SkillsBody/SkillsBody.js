177c129667cbedbbf88b028862242d944a043879
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-11-01 11:30:22 +0000

    turned SkillsBody into functional component and passed props with ES6 object destructure

--- src/Components/Skills/SkillsBody/SkillsBody.js
+++ src/Components/Skills/SkillsBody/SkillsBody.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './SkillsBody.scss';
 
-class SkillsBody extends Component {
-    render() { 
-        const { title, contents, books,  span, hasAuthor, open} = this.props;
-        let booksMarkUp = (
-            <div>
-                <h3>Finished</h3>
-                <ul>
-                    { hasAuthor ? 
-                    books.filter(a => !a.reading)
-                    .map((a, i) => <li key={i}>{a.title}<br /><span>{a.author}</span></li>) 
-                    : null}
-                </ul>
-                <h3>Reading</h3>
-                <ul>
-                    {hasAuthor ? 
-                    books.filter(a => a.reading)
-                    .map((a, i) => <li key={i}>{a.title}<br /><span>{a.author}</span></li>) 
-                    : null}
-                </ul>
-            </div>
-        );
-
-        let content = (
+const SkillsBody = ({ title, contents, books, hasAuthor, open}) => {
+    
+    let booksMarkUp = (
+        <div>
+            <h3>Finished</h3>
             <ul>
-                {(title !== 'Books') ? 
-                contents.map((a,i) => <li key={i}><span>{a.title + ' '}</span>{a.body}</li>) :
-                null}
+                { hasAuthor ? 
+                books.filter(a => !a.reading)
+                .map((a, i) => <li key={i}>{a.title}<br /><span>{a.author}</span></li>) 
+                : null}
             </ul>
-        );
-        
-        return ( 
-            <section className={open ? 'skills-content' : 'skills-content hide'}>    
-                {!hasAuthor ? content : null}
-                {hasAuthor ? booksMarkUp: null}
-            </section>
-        );
-    }
+            <h3>Reading</h3>
+            <ul>
+                {hasAuthor ? 
+                books.filter(a => a.reading)
+                .map((a, i) => <li key={i}>{a.title}<br /><span>{a.author}</span></li>) 
+                : null}
+            </ul>
+        </div>
+    );
+
+    let content = (
+        <ul>
+            {(title !== 'Books') ? 
+            contents.map((a,i) => <li key={i}><span>{a.title + ' '}</span>{a.body}</li>) :
+            null}
+        </ul>
+    );
+    
+    return ( 
+        <section className={open ? 'skills-content' : 'skills-content hide'}>    
+            {!hasAuthor ? content : null}
+            {hasAuthor ? booksMarkUp: null}
+        </section>
+    );
+    
 }
  
 
