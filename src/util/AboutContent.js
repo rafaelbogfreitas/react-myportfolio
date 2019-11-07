0493677c924cae3807b12255e08c886a3d93727b
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-11-07 15:39:44 -0300

    improved grammatically about text

--- src/util/AboutContent.js
+++ src/util/AboutContent.js
@@ -1,15 +1,15 @@
 const AboutContent = [
     `Hi, I'm Rafael, a self-taught web developer eager to learn and improve.`,
-    `Recently I grew to love the web and can't go one day without exercise my curiosity about it.
-    I am enthusiastic about CSS preprocessors and modern technologies as flex-box, grid and animations.
-    As I have a passionate interest in JavaScript and ES6, I spend most of my energy researching it and before
-    even considering to learn JS libraries or frameworks I was practicing and studying vanilla first. Most recently
-    my time is spent on learning React.`,
+    `Recently I grew to love the web and can't go one day without exercise my curiosity for it.
+    I am enthusiastic about CSS preprocessors and modern technologies such as flex-box, grid, and animations.
+    I have a passionate interest in learning JavaScript and the latest ES6+ specifications, as well as spending a lot of time 
+    researching new libraries and frameworks. Most recently I have been studying React, however, I was trying to get back to 
+    basics and understand the underlying JavaScript behind any new library or framework.`,
     `Nowadays, I have been focusing on getting better at organizing my code and making it as simple and semantic as possible,
     as my goal is to be part of a team and share my work with others.`,
     `Now I work as a floor manager in a busy London restaurant, where I have learned valuable life skills and work
-    ethics. I can meet up with deadlines, listen and accept critics, adapt positively to changes and work
-    with a team.`,
+    ethics. I can meet deadlines, listen and learn from criticism, adapt positively to changes and work
+    well in a team.`,
     `I have always been curious by nature and like challenges and puzzles. I am into cycling and have taken on the London to Paris
     route once but didn't enjoy the destination as much as I enjoyed the ride to get there.`,
     `Here you can see what I have achieved in my short journey in web development. I hope you enjoy!`
