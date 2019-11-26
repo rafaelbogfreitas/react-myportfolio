05430c84b2da92be3b92f894fe2a4bb9ec178f52
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-11-26 23:37:44 +0000

    added cv file and component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,11 +4,13 @@ import Skills from '../Skills/Skills';
 import AboutContent from '../../util/AboutContent';
 
 import './About.scss';
+import Curriculum from '../Curriculum/Curriculum';
 
  const About = () =>
     <div className="about">
         <div className={window.innerWidth <= 600 ? "about-wrap animated fadeInUp" : "about-wrap animated fadeIn"}>
         <h1>About</h1>
+            <Curriculum />
             {AboutContent.map((a, i) => <p key={i}>{a}</p>)}
         </div>
         <Skills />
---
+++ src/Components/Curriculum/Curriculum.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Curriculum = () => <a download="../src/images/rafael_freitas_cv.pdf" className="curriculum">Download CV</a>;
+
+export default Curriculum;
