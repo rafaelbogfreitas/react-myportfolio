7fc9ad58d8ccb624242bd7e88f4d80fbfc8f319e
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-24 23:08:50 -0300

    Translate About section to english

--- src/util/AboutContent.js
+++ src/util/AboutContent.js
@@ -1,35 +1,19 @@
 const AboutContent = [
     {
         ptbr: "Olá, meu nome é Rafael e sou um apaixonado por desafios. Desde que comecei minha jornada como desenvolvedor web, não passo um dia sem exercitar minhas habilidades e procurar desafios que estimulem meu aprendizado e me ajudem a solucionar problemas de maneira limpa e prática.",
-        en: "Hi, I'm Rafael, a self-taught web developer eager to learn and improve." 
+        en: "Hi, I'm Rafael, a Developer in love with challenges. Since I started my jorney in the Web Development path, I didn't go a day without practicing and taking on more challenges to help me grow and solve problems in a pragmatic way." 
     },
     {
-        ptbr: `Como autodidata, estudei X,Y e Z em Londres, cidade na qual residia e tive a possibilidade de aplicar esses conhecimentos nos projetos X, Y e Z que desenvolvi como freelancer em paralelo com meu trabalho como gerente do grupo de restaurantes Corbin&King.`,
-        en: `Recently I grew to love the web and can't go one day without exercise my curiosity for it.
-        I am enthusiastic about CSS preprocessors and modern technologies such as flex-box, grid, and animations.
-        I have a passionate interest in learning JavaScript and the latest ES6+ specifications, as well as spending a lot of time 
-        researching new libraries and frameworks. Most recently I have been studying React, however, I was trying to get back to 
-        basics and understand the underlying JavaScript behind any new library or framework.` 
+        ptbr: `Como autodidata, estudei HTML, CSS, JavaScript e React em Londres, cidade na qual residia e tive a possibilidade de aplicar esses conhecimentos em projetos que desenvolvi como freelancer em paralelo com meu trabalho como gerente do grupo de restaurantes Corbin&King.`,
+        en: `As an autodidact I studied HTML, CSS, JavaScript and React whilst living in London, city where I lived for seven years and had the oportunity to apply my knowledge on freelance projects in parallel with my job as a Manager in the renowned restaurant group Corbin&King.` 
     },
     {
-        ptbr: "Passados X meses nesse processo, percebi que meu amor por desenvolvimento Web era de fato genuíno e resolvi trabalhar por tempo integral como programador e aplicar todo meu tempo e esforço em aprimorar meus conhecimentos. Foi então que resolvi voltar para São Paulo e me inscrever no bootcamp da Iron Rack, renomado por X, Y e Z, para alcançar esse objetivo.",
-        en: `Nowadays, I have been focusing on getting better at organizing my code and making it as simple and semantic as possible,
-        as my goal is to be part of a team and share my work with others.` 
+        ptbr: "Passados 10 meses nesse processo, percebi que meu amor por desenvolvimento Web era de fato genuíno e resolvi trabalhar por tempo integral como programador e aplicar todo meu tempo e esforço em aprimorar meus conhecimentos. Foi então que resolvi voltar para São Paulo e me inscrever no bootcamp global da Ironhack, renomado por Desenvolvimento Fullstack com Javascript, para alcançar esse objetivo.",
+        en: `After about ten months in the process of lerning web development, I came to realized that my love for the profession is genuine and I decided to dedicate my time fully to become a programmer and to improve my knowledge. This was when I made a big decision in my life to go back to live in São Paulo, where I was born, and join the global bootcamp Ironhack, famous for teaching fullstack development with JavaScript, to help me achieve my goals.` 
     },
     {
         ptbr: "Atualmente, com o bootcamp e muitos projetos concluídos com sucesso, estou pronto para entrar no mercado de trabalho brasileiro e solucionar novos desafios.",
-        en: `Now I work as a floor manager in a busy London restaurant, where I have learned valuable life skills and work
-        ethics. I can meet deadlines, listen and learn from criticism, adapt positively to changes and work
-        well in a team.` 
-    },
-    {
-        ptbr: "",
-        en: `I have always been curious by nature and like challenges and puzzles. I am into cycling and have taken on the London to Paris
-        route once but didn't enjoy the destination as much as I enjoyed the ride to get there.` 
-    },
-    {
-        ptbr: "",
-        en: `Here you can see what I have achieved in my short journey in web development. I hope you enjoy!` 
+        en: `Nowadays, having finished susccessfully the course and developed a few projects, I am ready to become party of the brazilian tech industry and to help solving problems. ` 
     }
 ];
 
